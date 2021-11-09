9a727dc91e5056d35b8bb5e5a7738518b01c68fd
AbuThekky <82602169+AbuThekky@users.noreply.github.com>
2021-11-09 12:49:26 +0000

    Add files via upload

---
+++ Runner.java
@@ -0,0 +1,10 @@
+package flowchart;
+
+public class Runner {
+	public static void main(String[] args) {
+
+		flowchart.number(1, 2, false);
+		Logic.main(5000);
+
+	}
+}
---
+++ flowchart.java
@@ -0,0 +1,14 @@
+package flowchart;
+
+public class flowchart {
+	public static void number(int a, int b, boolean sum) {
+		if (sum) {
+			System.out.println(a + b);
+		}
+		if (!sum) {
+			System.out.println(a * b);
+		}
+
+	}
+
+}
